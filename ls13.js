5ee5ff2206f86193b22e7cee71dad162b6c1e707
theChrisElson <32608072+thechriselson@users.noreply.github.com>
2021-04-08 13:02:11 +0100

    Update and rename ls12.js to ls13.js

--- ls13.js
+++ ls13.js
@@ -12,12 +12,12 @@ function lsActiveInactive(e, x) {
 	else {s1 = inac; s2 = ac}
 	if(s1 !== undefined) {
 		if(s1.length >= 2) {
-			if(s1[0] == "class") {e.classList.remove(s1[1])}
+			if(s1[0] == "class") {console.log("ACTIVE"); e.classList.remove(s1[1])}
 		}
 	}
 	if(s2 !== undefined) {
 		if(s2.length >= 2) {
-			if(s2[0] == "class") {e.classList.add(s2[1])}
+			if(s2[0] == "class") {console.log("INACTIVE"); e.classList.add(s2[1])}
 			else if(s2[0] == "int") {int = s2[1]}
 		}
 	}
